dc31e0fe97c799ddb696049deed539df83779dd4
deaaprizal <afrizalmiphone@gmail.com>
2019-01-24 10:25:22 +0700

    ui fixing on button sign in

--- component/pages/form-user/login.js
+++ component/pages/form-user/login.js
@@ -122,9 +122,6 @@ export default class Register extends Component {
 
   render() {
     const { warning } = this.state;
-    const areaRegisterButtonResponsive = {
-      padding: "1%"
-    };
     return (
       <div>
         <Container>
@@ -143,10 +140,10 @@ export default class Register extends Component {
             <Grid.Column style={{ maxWidth: 450 }}>
               <Divider hidden />
               <Divider hidden />
-              <Header as="h3">
-                <Icon name="user circle" size="large" />
-                Login Account
+              <Header as="h1" color="orange">
+                <i>enjoy your Way!</i>
               </Header>
+              <Divider hidden/>
               <Form size="large" onSubmit={this.handleSubmit}>
                 <Segment stacked>
                   <Form.Input
@@ -173,29 +170,21 @@ export default class Register extends Component {
                 </Segment>
               </Form>
             </Grid.Column>
-            <Grid.Column
-              verticalAlign="middle"
-              style={areaRegisterButtonResponsive}
-            >
-              <Button
-                onClick={this.googleSignin.bind(this)}
-                content="Sign in with Google"
-                color="google plus"
-                icon="google"
-                size="mini"
-              />
-              <Button
-                content="Sign in with Facebook"
-                icon="facebook"
-                color="facebook"
-                size="mini"
-              />
+            <Grid.Column>
+              <Message>                
+                New to Way ? <a href="#/register"><i>Sign Up Here !</i></a>
+                <Divider horizontal>Or</Divider>
+                <Button
+                  onClick={this.googleSignin.bind(this)}
+                  content="Sign in with Google"
+                  color="google plus"
+                  icon="google"
+                  size="small"
+                  fluid
+                />
+              </Message>
             </Grid.Column>
           </Grid>
-          <Divider />
-          <Message>
-            New to Sleeperbox ? <a href="#/register">Sign Up Here ! </a>
-          </Message>
         </Container>
       </div>
     );
