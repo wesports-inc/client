c3a5ebc3b4d346484f3800d90d82d4baa18d5dab
teday <tedi.suryadi77@gmail.com>
2019-02-16 13:42:05 +0700

    Search People

--- component/pages/category/people/allPeople.js
+++ component/pages/category/people/allPeople.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Container, Grid, Divider, Image, List, Header, Button, Modal } from 'semantic-ui-react';
+import { Container, Grid, Divider, Image, List, Header, Button, Modal, Input, Statistic } from 'semantic-ui-react';
 import Skeleton from 'react-skeleton-loader';
 import axios from 'axios';
 
@@ -15,9 +15,12 @@ export default class allPeople extends Component {
             email_friend: '',
             open: false,
             fotos: '',
-            allfoto: []
+            allfoto: [],
+            cari: "",
+            kode: 0
         };
         this.generateSkeleton = this.generateSkeleton.bind(this)
+        this.handlePost = this.handlePost.bind(this);
     }
 
     componentWillMount() {
@@ -54,19 +57,20 @@ export default class allPeople extends Component {
     }
 
     componentDidUpdate(prevProps, prevState) {
-        if(!prevState.email_friend){
-        axios({
-            method: 'post',
-            url: '/api/profile',
-            headers: { 
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            data: {
-                email: this.state.email_friend
-            }, // This is the body part
-          }).then(result => this.setState({friendship: result.data}))
-        }
+        if (this.state.kode == 1) {
+            axios({
+              method: "post",
+              url: "/api/search",
+              headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+              },
+              data: {
+                username: this.state.cari,
+                email: this.state.email
+              }
+            }).then(result => this.setState({ datas: result.data, kode: 0 }));
+          }
         const {isLogin} = this.state;
         if(isLogin === false){ 
             window.location = '#/login'
@@ -120,6 +124,16 @@ export default class allPeople extends Component {
         </div>
     }
 
+    handlePost(event) {
+        let target = event.target;
+        let value = target.value;
+        let name = target.name;
+        this.setState({
+          [name]: value,
+          kode: 1
+        })
+      }
+
     loop() {
         const test = []
         const {allfoto} = this.state
@@ -132,17 +146,56 @@ export default class allPeople extends Component {
         window.location='#/user/profile';
     }
 
+    generateZeroData() {
+        const divConten = {
+          marginTop: "40%",
+          marginBottom: "60%"
+        };
+        return (
+          <div style={divConten}>
+            <Header as="h2" icon textAlign="center">
+              <Image
+                centered
+                size="large"
+                src="https://image.spreadshirtmedia.com/image-server/v1/mp/designs/12346806,width=178,height=178/cute-devil.png"
+              />
+              <Header.Content>
+                <Statistic>
+                  <Statistic.Value text>Hell Yeah,</Statistic.Value>
+                  <Statistic.Label>
+                  </Statistic.Label>
+                  <Statistic.Label>No Result</Statistic.Label>
+                </Statistic>
+              </Header.Content>
+            </Header>
+          </div>
+        );
+      }
+
     close = () => this.setState({ open: false, email_friend: '' }, () => sessionStorage.removeItem('username'))
     
     render() {
         const { datas, isLoading, friendship, open, dimmer, fotos, allfoto } = this.state
         return (
             <div style={{marginBottom: 45}}>
-            {isLoading ? this.generateSkeleton() :
+            <Input
+                fluid
+                icon="search"
+                onChange={this.handlePost}
+                name="cari"
+                value={this.state.cari}
+                placeholder="Seacrh"
+            />
+            <br />
+            {datas.length === 0 ? (
+                this.generateZeroData()
+              ) : (
+            isLoading ? this.generateSkeleton() :
             <Container>
                 {datas.map(data => {  
                     return (
                 <Grid columns={1} key={data._id}>
+                    { data.email === this.state.email ? null :
                     <Grid.Column>
                         <List verticalAlign="middle" onClick={() => {this.handleClick(data.email)}}>
                             <List.Item>
@@ -154,6 +207,7 @@ export default class allPeople extends Component {
                             </List.Item>
                         </List>
                     </Grid.Column>
+                    }
                 </Grid>
                 ); })}
                 <Modal dimmer={dimmer} open={open} onClose={this.close} closeIcon>
@@ -182,7 +236,7 @@ export default class allPeople extends Component {
             </Modal.Actions>
         </Modal>
             </Container>
-            }
+              )}
             </div>
         );
     }
