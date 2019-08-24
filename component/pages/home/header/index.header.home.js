6ad8c76c48d2d27b161533e28eef5c7b6edc26a0
unluckymind <unluckymind2019@gmail.com>
2019-08-24 12:16:06 +0700

    setup structure

---
+++ component/pages/home/header/index.header.home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Headers = () => {
+  return (
+    <div>
+      <p>Header</p>
+    </div>
+  );
+};
+
+export default Headers;
---
+++ component/pages/static/index.static.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <p>navbar</p>
+    </div>
+  );
+};
+
+export const Footer = () => {
+  return (
+    <div>
+      <p>footer</p>
+    </div>
+  );
+};
